d3677e8989e11a08d912057ef26afdabfbfd9892
Mark Livingstone <kramlivingstone@gmail.com>
2019-11-30 17:37:58 +0800

    Updates

---
+++ precache-manifest.344378bc3fe5ae39e03e65af60dd4412.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2104cccb75aa4c3f2bd432db1828f85",
+    "url": "/users-rolodex/index.html"
+  },
+  {
+    "revision": "6d085bf92ea516781a62",
+    "url": "/users-rolodex/static/css/main.a9f34bc4.chunk.css"
+  },
+  {
+    "revision": "26263cf867999119676f",
+    "url": "/users-rolodex/static/js/2.d2d5235b.chunk.js"
+  },
+  {
+    "revision": "6d085bf92ea516781a62",
+    "url": "/users-rolodex/static/js/main.661d17d3.chunk.js"
+  },
+  {
+    "revision": "648f29a0958b0ccb4d2f",
+    "url": "/users-rolodex/static/js/runtime-main.f0dfc033.js"
+  }
+]);
